f5d7cb588f72f5caa90f9a29203a40880cd4b4e1
arthurlee <boyal2008@gmail.com>
2016-09-27 22:47:03 +0800

    simple hello world of http server

--- goa.go
+++ goa.go
@@ -3,8 +3,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "[Goa] Hello, world!")
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", handleHello)
+
+	port := ":5400"
+	log.Println("Goa start at port", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
