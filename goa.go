9f9126a0d43695fa64ebc98cb87e8c78890dfd72
arthurlee <boyal2008@gmail.com>
2016-10-02 13:12:25 +0800

    return will json array

--- goa.go
+++ goa.go
@@ -9,18 +9,32 @@ import (
 )
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
 	person := struct {
-		Name string `json:"name"`
+		Name string `json:"name", db:"pk"`
 		Age  int    `json:"age"`
 	}{"Tom", 12}
 
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(person)
+}
+
+func handleCourseList(w http.ResponseWriter, r *http.Request) {
+	courses := []struct {
+		CourseId      int    `json:"courseId"`
+		CourseName    string `json:"courseName"`
+		CourseSummary string `json:"courseSummary"`
+	}{
+		{1, "Math", "Math for grade 6"},
+		{2, "Art", "Art for grade 6"},
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
 }
 
 func main() {
 	http.HandleFunc("/", handleHello)
+	http.HandleFunc("/course/list", handleCourseList)
 
 	port := ":5400"
 	log.Println("Goa start at port", port)
--- readme.md
+++ readme.md
@@ -3,3 +3,9 @@ goa
 Goa is a light-weight restful api framework using golang. It only support json format.
 
 ### under construction...
+
+# Installation
+
+Follow the steps below:
+- go get github.com/go-sql-driver/mysql
+- 
