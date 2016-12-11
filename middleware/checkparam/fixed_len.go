b58ff937abd4d8a19e075f7ccfea25b977969169
arthurlee <boyal2008@gmail.com>
2016-12-11 22:52:58 +0800

    add fixed_len check

---
+++ middleware/checkparam/fixed_len.go
@@ -0,0 +1,43 @@
+package checkparam
+
+import (
+	"errors"
+	"fmt"
+	"github.com/arthurlee/goa/server"
+	"strings"
+)
+
+//
+// check if the parameter is exist, is specific length
+//
+
+type IFixedLen interface {
+	GetFixedLen() int
+}
+
+type FixedLenCheckItem struct {
+	CheckItem
+	fixedLen int
+}
+
+func (me *FixedLenCheckItem) GetFixedLen() int {
+	return me.fixedLen
+}
+
+func HandlerFixedLen(item CheckBase, ctx *server.HttpContext) error {
+	name := item.GetName()
+
+	val := strings.Join(ctx.Form[name], "")
+	ctx.Log.Debug("HandlerFixedLen: %s = %s", name, val)
+
+	fixedLen := item.(IFixedLen)
+	if len(val) != fixedLen.GetFixedLen() {
+		return errors.New(fmt.Sprintf("parameter %s's length is not %d", name, fixedLen.GetFixedLen()))
+	}
+
+	return nil
+}
+
+func FixedLen(name string, errorCode string, length int) CheckBase {
+	return &FixedLenCheckItem{CheckItem{name, errorCode, HandlerFixedLen}, length}
+}
